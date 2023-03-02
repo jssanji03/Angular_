532cb09e31eefb51db3908c3b5272349f56ec522
jess <jesssung@soetek.com.tw>
2023-03-02 12:09:13 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AlertModule,SidebarModule } from '@coreui/angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import containers
+import {
+  DefaultLayoutComponent,
+} from './default-layout';
+const APP_CONTAINERS = [
+  DefaultLayoutComponent,
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,...APP_CONTAINERS
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule,
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/default-layout/index.ts
@@ -0,0 +1,3 @@
+// export * from './default-foo/default-footer.component';
+// export * from './default-headerter/default-header.component';
+export * from './default-layout.component';
